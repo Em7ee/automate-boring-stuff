70d821a5bdeee3b287848f2076b2d2f72c1e1c30
Matt <matt@matttopor.com>
2017-02-01 02:57:20 -0500

    This is a push of a python script written from Automate the Boring Stuff.

---
+++ yourName.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3.5
+
+# Small program that will keep asking you to type your name.
+
+name = ''
+
+while name != 'your name':
+    print('Please type your name.')
+    name = input()
+print('Thank You!')
